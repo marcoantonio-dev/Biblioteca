1e896eb5c7f265a0272cc01bbd08ba7b50889131
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-05-22 15:37:44 -0300

    iniciando projeto

---
+++ Biblioteca/Models/EmprestimoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Biblioteca.Models
+{
+    public class EmprestimoModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime Recebedor { get; set; } = DateTime.Now;
+    }
+}
